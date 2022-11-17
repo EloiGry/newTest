d2172b9be09ee4174b9df17b0bc95b9026da5380
EloiGry <eloi.grychta@gmail.com>
2022-11-17 18:49:13 +0100

    modif

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,22 +5,19 @@ export default function Home() {
   const [keyBoard, setKeyBoard] = useState(false)
   const [text, setText] = useState([])
   const keyLayout = [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-    "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-    "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "<-",
+    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
     "space"
 ];
 
 
 const handleChange = (letter) => {
-  // setText([...text, letter])
   switch(letter) {
-    case 'backspace':
+    case '<-':
        setText(text.splice(0, text.length-1))
        break;
     case 'space' :
-      setText([...text, ""])
+      setText([...text, " "])
       break;
     default:
       setText([...text, letter])
@@ -31,10 +28,10 @@ const handleChange = (letter) => {
     <div>
       <textarea onFocus={() => setKeyBoard(true)} className="border-2 border-black" value={text.join('')}></textarea>
       {keyBoard && 
-        <div className="bg-green-800 w-full min-h-1/2 fixed bottom-0 text-white flex flex-wrap">
+        <div className="bg-[#004134] w-full min-h-1/2 fixed bottom-0 text-white flex flex-wrap">
         {keyLayout.map((letter,index) => {
           return (
-            <button key={index} onClick={() => {handleChange(letter)}} className="px-4 py-2 m-1 cursor-pointer">
+            <button key={index} onClick={() => {handleChange(letter)}} className="w-1/12 px-4 py-2 m-1 cursor-pointer bg-[#3B675D]">
               {letter}
             </button>
           )
